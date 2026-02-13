302bfa97f2b922589c1b6d31641ae95ad3b8216c
TLSDNPrasanna <prasannatripurari21@gmail.com>
2026-02-13 21:19:08 +0530

    Pushed the python file

---
+++ game_score.py
@@ -0,0 +1,9 @@
+name=input("Enter the Player's name: ")
+number=int(input("Enter the number of games played: "))
+score=int(input("Enter the total score achieved: "))
+average=score/number
+
+print(f"Player: {name}")
+print(f"Games Played: {number}")
+print(f"Total Score: {score}")
+print(f"Average Score: {average}")
